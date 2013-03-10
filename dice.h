a579dbae4c3acfa79708707518b78daa81eab5f6
Plazma <nick.allgood@gmail.com>
2013-03-10 10:10:17 -0700

    Create dice.h

---
+++ dice.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#ifndef DICE_H
+#define DICE_H
+
+void getHelp();
+void loadDice();
+void displayDice(char *dice[]);
+int rollDice(char *dice[]);
+
+#endif
